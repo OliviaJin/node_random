1c45798d18f2d23b7a32347a0f1ee02742472381
Yunuuo <jiiny738@newschool.edu>
2023-11-22 05:35:03 -0500

    6

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3001;
-const getRandomCat = require('random-cat-img');
+import express from 'express';
+import uniqueString from 'unique-string';
+const app = express()
 
-(async () => {
-    const data = await getRandomCat();
-    console.log(data.message);
-})();
+// Set the port the application will be running on
+const port = process.env.PORT || 3001
 
-getRandomCat().then(data => {
-    console.log(data.message);
-    // Handle the object here, if it's part of the response or logic
-    const responseObject = {
-        "success": true,
-        "status": 200,
-        "info": { "category": "animals", "endpoint": "cat" },
-        "message": "https://cdn.sefinek.net/images/animals/cat/neva-masquerade-cats-1375033-min.jpg"
-    };
-    // Do something with responseObject
-});
+app.get('/', (req, res) => {
+  const randamUniqueString = (uniqueString)
+
+  res.send(uniqueString)
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
