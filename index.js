4a2c523e431e7dd80bb1c97155f68aa7162ee2e4
Yunuuo <jiiny738@newschool.edu>
2023-11-22 05:15:59 -0500

    4

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
-
-app.get('/', (req, res) => {
-  res.send("Write an instruction for a drawing here. For example: draw a line across the page, draw 10 circles of different sizes, draw a duck.");
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
-
 const getRandomCat = require('random-cat-img');
 
 (async () => {
@@ -27,4 +18,8 @@ getRandomCat().then(data => {
         "message": "https://cdn.sefinek.net/images/animals/cat/neva-masquerade-cats-1375033-min.jpg"
     };
     // Do something with responseObject
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
